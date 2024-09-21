cba3fe3ea2b44d3022ee9d6098cceb9608c846dc
parisollie <pjff18@gmail.com>
2024-09-20 21:14:13 -0600

    first commit

---
+++ js/01.js
@@ -0,0 +1,24 @@
+
+/************************************************** Vid 06 *************************************************/
+
+// Variables - let 
+/*var ya no se usa , solo let y const
+Es un lenguaje tipo dinámico, una variable puede contener diferentes tipos de datos, es decir una variable 
+puede contener diferentes tipos de datos ,porque el tipo de dato se almacena en el valor de la variable y no en la variable como tal */
+
+let cliente = "Paul"
+let precioDescuento = 220 // En JS se usa camelCase.
+
+// Reasiganmos un nuevo valor. 
+cliente = 20
+cliente = true
+
+// Pueden iniciar sin un valor.
+let precio
+//Despúes podriamos ponerle un valor a lo largo del progama.
+precio = 1000
+
+console.log(precio)
+console.log(cliente)
+console.log(precioDescuento)
+console.log('Hola Mundo')
